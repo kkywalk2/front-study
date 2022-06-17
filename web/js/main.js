45fb37ff0605024108a2b9dc019e3d7ba44919e9
kkywalk2 <kkywalk2@gmail.com>
2022-06-17 15:47:33 +0900

    20220617

--- web/js/main.js
+++ web/js/main.js
@@ -1,9 +1,20 @@
-const box = document.querySelector(".class-card")
-const originalColor = box.style.backgroundColor
-box.addEventListener("mouseover", () => {
-   box.style.backgroundColor = "red"
-})
+const upButton = document.getElementById('up-button')
 
-box.addEventListener("mouseout", () => {
-    box.style.backgroundColor = originalColor
- })
+const checkScroll = () => {
+   if( scrollY == 0 ) {
+      upButton.style.visibility = 'hidden'
+   } else {
+      upButton.style.visibility = 'visible'
+   }
+}
+
+const moveToTop = () => {
+   scrollTo({
+      top: 0,
+      behavior: 'smooth'
+   })
+}
+
+window.addEventListener('scroll', checkScroll)
+upButton.addEventListener('click', moveToTop)
+
